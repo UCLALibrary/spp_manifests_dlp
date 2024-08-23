0b922a4be360b409be8ade3bbfe3dae1bd81fbb9
Ngozi Harrison <ngozih@g.ucla.edu>
2024-08-23 09:49:46 -0700

    fixed spacing issue

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ df = pd.read_csv("csv/file_names_to_remove.csv")
 df["Status"] = ""
 manifest_files = df["Manifest_File"].drop_duplicates()
 
-for row in manifest_files[:2]:
+for row in manifest_files[0:2]:
     path_to_file = f"{row}"
     files_to_remove = df.loc[df["Manifest_File"] == row]["File_name_cleanup"]
     with open(path_to_file, 'r') as f:
@@ -13,14 +13,13 @@ for row in manifest_files[:2]:
     with open(path_to_file, 'w') as f:
         for canvas in j["sequences"][0]["canvases"]:
             items = canvas["images"][0]["resource"]["item"]
-            for i in range(len(items)-1):
-                #print(items[i]["label"])
+            for i in reversed(range(len(items)-1)):
                 label = items[i]["label"]
                 if (label in files_to_remove.values):
                     del items[i]
                     row_index = df.query("Manifest_File == @row and File_name_cleanup == @label").index[0]
                     df.loc[row_index, "Status"] = "Success"
         # you would replace the following line with saving the file using the same file name
-        json.dump(j, f, indent=2)
+        json.dump(j, f, indent=2, separators=(',', ' : '))
 
 df.to_csv("csv/file_names_to_remove.csv", index = False)
