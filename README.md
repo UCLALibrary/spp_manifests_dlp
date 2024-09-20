355c0a401beec77569280c145398b133f16dbdac
Ngozi Harrison <ngozih@g.ucla.edu>
2024-09-20 09:15:29 -0700

    updated readme and folders

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # spp_manifests_dlp
+
+This script takes a list of files and folder of json manifests and removes those file names from the manifest files
+
+
+### Set up
+**Requirements**
+- python3.6
+- pandas
+- numpy
+
+### Usage
+
+```python
+python3 remove_from_manifests.py
+```
+
+The script will then prompt you for the path to the csv file containing filenames to remove and the path to the folder containing manifests
+
+```python
+Enter path to csv containing file names to remove: /path/to/file.csv
+Enter path to folder containing manifests: /path/to/folder
+```
+
--- remove_from_manifests.py
+++ remove_from_manifests.py
@@ -1,11 +1,22 @@
-import pandas as pd, json, sys, numpy as np, os
+import pandas as pd, json, sys
 
-df = pd.read_csv("csv/file_names_to_remove.csv")
+path_to_csv = sys.argv[1]#input("Enter path to csv containing file names to remove: ")
+path_to_manifests = sys.argv[2]#input("Enter path to folder containing manifests: ")
+
+# import filenames to delete
+df = pd.read_csv(f"{path_to_csv}")
+
+# create new column for 
 df["Status"] = ""
+
+# create list of unique manifest files
 manifest_files = df["Manifest_File"].drop_duplicates()
 
+
+
+# iterate over manifest files and remove item if label name is in list of files to delete
 for row in manifest_files:
-    path_to_file = f"{row}"
+    path_to_file = f"{path_to_manifests}/{row}"
     files_to_remove = df.loc[df["Manifest_File"] == row]["File_name_cleanup"]
     with open(path_to_file, 'r') as f:
         j = json.load(f)
@@ -19,7 +30,7 @@ for row in manifest_files:
                     del items[i]
                     row_index = df.query("Manifest_File == @row and File_name_cleanup == @label").index[0]
                     df.loc[row_index, "Status"] = "Success"
-        # you would replace the following line with saving the file using the same file name
+        # save file
         json.dump(j, f, indent=2, separators=(',', ' : '))
 
 df.to_csv("csv/file_names_to_remove.csv", index = False)
